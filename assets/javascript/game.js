568d4137c2e13d56b0baa02978719ef450a967ba
Juan Carlos Lenz <jclenz@miserablepop.local>
2019-09-19 21:38:35 -0700

    removed character divs from index.html. created a function to create character divs in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,88 @@
-// Defining character objects
+$(document).ready(function() {
 
-var bobaFett = {
-    nickname: "boba",
-    name: "Boba Fett",
-    healthPoints: 110,
-    attackPoints: 20,
-    counterPoints: 15,
-    img: '<img src="assets/images/bobafett.png" class="image" >'
-};
-
-var hanSolo = {
-    nickname: "han",
-    name: "Han Solo",
-    healthPoints: 150,
-    attackPoints: 25,
-    counterPoints: 20,
-    img: '<img src="assets/images/hansolo.png" class="image" >'
-};
-
-var lobot = {
-    nickname: "lobot",
-    name: "Lobot",
-    healthPoints: 100,
-    attackPoints: 10,
-    counterPoints: 10,
-    img: '<img src="" class="image" >'
-};
-
-var lando = {
-    nickname: "lando",
-    name: "Lando Calrissian",
-    healthPoints: 120,
-    attackPoints: 15,
-    counterPoints: 12,
-    img: '<img src="" class="image" >'
-};
+    // VARIABLES
+    ///////////////////////////////////////////////////////////////////////////////
 
 
-// Setting characters array
-var characters = [bobaFett, hanSolo, lobot, lando]
+    // defining character objects
+
+    // boba fett object
+    var bobaFett = {
+        nickname: 'boba',
+        charName: 'Boba Fett',
+        healthPoints: 110,
+        attackPoints: 20,
+        counterPoints: 15,
+        charImage: '<img src="assets/images/characters/bobafett.png" class="image" >'
+    };
+
+    // han solo object
+    var hanSolo = {
+        nickname: 'han',
+        charName: 'Han Solo',
+        healthPoints: 150,
+        attackPoints: 25,
+        counterPoints: 20,
+        charImage: '<img src="assets/images/characters/hansolo.png" class="image" >'
+    };
+
+    // lobot object
+    var lobot = {
+        nickname: 'lobot',
+        charName: 'Lobot',
+        healthPoints: 100,
+        attackPoints: 10,
+        counterPoints: 10,
+        charImage: '<img src="assets/images/characters/lobot.png" class="image" >'
+    };
+
+    // lando object
+    var lando = {
+        nickname: 'lando',
+        charName: 'Lando Calrissian',
+        healthPoints: 120,
+        attackPoints: 15,
+        counterPoints: 12,
+        charImage: '<img src="assets/images/characters/lando.png" class="image" >'
+    };
+
+    
+
+    // Setting character objects array
+    var charactersObjects = [bobaFett, hanSolo, lobot, lando];
+    
+    var characters = [];
+
+
+    // FUNCTIONS
+    ///////////////////////////////////////////////////////////////////////////////
+
+    function startGame (){
+        createCharacters(charactersObjects);
+    };
+
+
+    function createCharacters (arg){
+        if (arg.length === 4){
+            for (var i=0; i < arg.length; i++){
+                var $attacker = $('<div id='+arg[i].nickname+'>');
+                $attacker.append('<div class="characterName">'+ arg[i].charName);
+                $attacker.append(arg[i].charImage);
+                $attacker.append('<div class="characterHealth">'+arg[i].healthPoints);
+                $attacker.attr('class', 'character col-md-3');
+
+                characters.push(arg[i].nickname);
+                $('#characters').append($attacker);
+
+            }
+        }
+        
+    };
+
+    
+
+
+
+
+    startGame();
+});
