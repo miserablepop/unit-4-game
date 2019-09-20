1574d4bc97d887433ed9d635b194924958c17a5f
Juan Carlos Lenz <jclenz@miserablepop.local>
2019-09-19 18:11:32 -0700

    Pushing updates to the index.html (changed background image and bootstrap grid), new images, create character objects in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+// Defining character objects
+
+var bobaFett = {
+    nickname: "boba",
+    name: "Boba Fett",
+    healthPoints: 110,
+    attackPoints: 20,
+    counterPoints: 15,
+    img: '<img src="assets/images/bobafett.png" class="image" >'
+};
+
+var hanSolo = {
+    nickname: "han",
+    name: "Han Solo",
+    healthPoints: 150,
+    attackPoints: 25,
+    counterPoints: 20,
+    img: '<img src="assets/images/hansolo.png" class="image" >'
+};
+
+var lobot = {
+    nickname: "lobot",
+    name: "Lobot",
+    healthPoints: 100,
+    attackPoints: 10,
+    counterPoints: 10,
+    img: '<img src="" class="image" >'
+};
+
+var lando = {
+    nickname: "lando",
+    name: "Lando Calrissian",
+    healthPoints: 120,
+    attackPoints: 15,
+    counterPoints: 12,
+    img: '<img src="" class="image" >'
+};
+
+
+// Setting characters array
+var characters = [bobaFett, hanSolo, lobot, lando]
